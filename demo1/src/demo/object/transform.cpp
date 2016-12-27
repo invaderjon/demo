d063386cbec8e0ab9590ed529aa6d49b2c7b713f
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2016-12-27 15:51:47 -0500

    Define transformation class.

--- demo1/src/demo/object/transform.cpp
+++ demo1/src/demo/object/transform.cpp
@@ -1,1 +1,57 @@
 #include "transform.h"
+
+namespace demo
+{
+
+namespace obj
+{
+
+// MUTATOR FUNCTIONS
+void Transform::setEulerRotation( const glm::vec3& rotation )
+{
+    glm::quat yaw = glm::rotate( glm::quat(), rotation.z,
+                                 glm::vec3( 0.0f, 0.0f, 1.0f ) );
+
+    glm::quat pitch = glm::rotate( glm::quat(), rotation.x,
+                                   glm::vec3( 1.0f, 0.0f, 0.0f ) );
+
+    glm::quat roll = glm::rotate( glm::quat(), rotation.y,
+                                  glm::vec3( 0.0f, 1.0f, 0.0f ) );
+
+    _rotation = yaw * pitch * roll;
+    recomputeMatrix();
+}
+
+// MEMBER FUNCTIONS
+void Transform::rotateEuler( const glm::vec3& rotation )
+{
+    glm::quat rot = glm::rotate( _rotation, rotation.z,
+                                 glm::vec3( 0.0f, 0.0f, 1.0f ) );
+
+    rot = glm::rotate( rot, rotation.x, glm::vec3( 1.0f, 0.0f, 0.0f ) );
+    rot = glm::rotate( rot, rotation.y, glm::vec3( 0.0f, 1.0f, 0.0f ) );
+
+    _rotation = rot;
+    recomputeMatrix();
+}
+
+// HELPER FUNCTIONS
+void Transform::recomputeMatrix()
+{
+    glm::mat4 trans;
+    trans[3][0] = _position.x;
+    trans[3][1] = _position.y;
+    trans[3][2] = _position.z;
+
+    glm::mat4 rot( glm::mat4_cast( _rotation ) );
+
+    glm::mat4 scale( _scale );
+    scale[3][3] = 1.0f;
+
+    _matrix = trans * rot * scale;
+    _hasChanged = true;
+}
+
+} // End nspc obj
+
+} // End nspc demo
--- demo1/src/demo/object/transform.h
+++ demo1/src/demo/object/transform.h
@@ -1,8 +1,19 @@
 //
 // Object transformation matrix.
 //
+// The transformations are applied in the following order:
+// scale
+// rotation
+// translation
+//
+// Rotating about a point affects both scale and rotation.
+// Changing the rotation directly will not effect position.
+//
 #ifndef DEMO_TRANSFORM_H
 #define DEMO_TRANSFORM_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace demo
 {
@@ -12,6 +23,39 @@ namespace obj
 
 class Transform
 {
+  private:
+    // MEMBERS
+    /**
+     * The transformation matrix.
+     */
+    glm::mat4 _matrix;
+
+    /**
+     * The rotation.
+     */
+    glm::quat _rotation;
+
+    /**
+     * The position.
+     */
+    glm::vec3 _position;
+
+    /**
+     * The scale.
+     */
+    float _scale;
+
+    /**
+     * Whether or not the transform has changed.
+     */
+    bool _hasChanged;
+
+    // HELPER FUNCTIONS
+    /**
+     * Recompute the transformation matrix.
+     */
+    void recomputeMatrix();
+
   public:
     // CONSTRUCTORS
     /**
@@ -39,10 +83,30 @@ class Transform
 
     // ACCESSOR FUNCTIONS
     /**
+     * Get the transformation matrix.
+     * @return The matrix.
+     */
+    const glm::mat4& matrix() const;
+
+    /**
      * Get the position.
      * @return The position.
      */
     const glm::vec3& position() const;
+
+    /**
+     * Get the rotation.
+     * @return The rotation.
+     */
+    const glm::quat& rotation() const;
+
+    /**
+     * Get the euler rotation angles.
+     *
+     * This is pitch, roll, and yaw in that order.
+     * @return The euler rotation.
+     */
+    glm::vec3 eulerRotation() const;
 
     /**
      * Get the forward vector.
@@ -61,7 +125,247 @@ class Transform
      * @return The unit directional vector.
      */
     glm::vec3 right() const;
+
+    /**
+     * Get the scale.
+     * @return The scale.
+     */
+    float scale() const;
+
+    /**
+     * Check if the transform has changed since the flag was last reset.
+     * @return Has it changed?
+     */
+    bool hasChanged() const;
+
+    // MUTATOR FUNCTIONS
+    /**
+     * Set the position.
+     * @param position The position.
+     */
+    void setPosition( const glm::vec3& position );
+
+    /**
+     * Set the rotation.
+     * @param rotation The rotation.
+     */
+    void setRotation( const glm::quat& rotation );
+
+    /**
+     * Set the rotation around the z axis, x axis, and then y axis in that
+     * order.
+     * @param x The pitch in degrees.
+     * @param y The roll in degrees.
+     * @param z The yaw in degrees.
+     */
+    void setEulerRotation( float x, float y, float z );
+
+    /**
+     * Sets the rotation around the z axis, x axis, and then y axis in that
+     * order.
+     * @param rotation The pitch, roll, and yaw in degrees.
+     */
+    void setEulerRotation( const glm::vec3& rotation );
+
+    /**
+     * Set the scale.
+     * @param scale The scale.
+     */
+    void setScale( float scale );
+
+    // MEMBER FUNCTIONS
+    /**
+     * Translate the transform.
+     * @param translation The translation.
+     */
+    void translate( const glm::vec3& translation );
+
+    /**
+     * Rotate the transform around its center point.
+     * @param rotation The rotation.
+     */
+    void rotate( const glm::quat& rotation );
+
+    /**
+     * Rotate around the z axis, x axis, and then y axis in that order.
+      * @param x The pitch in degrees.
+      * @param y The roll in degrees.
+      * @param z The yaw in degrees.
+      */
+    void rotateEuler( float x, float y, float z );
+
+    /**
+     * Rotate around the z axis, x axis, and then y axis in that order.
+     * @param rotation The pitch, roll, and yaw in degrees.
+     */
+    void rotateEuler( const glm::vec3& rotation );
+
+    /**
+     * Scale the transform.
+     *
+     * This does not effect translation.
+     *
+     * @param scale Scale the transform.
+     */
+    void scale( float scale );
+
+    /**
+     * Reset the hasChanged flag.
+     */
+    void resetChangedFlag();
 };
+
+// CONSTRUCTORS
+inline
+Transform::Transform()
+{
+}
+
+inline
+Transform::Transform( const Transform& other )
+    : _matrix( other._matrix ), _position( other._position ),
+      _scale( other._scale ), _rotation( other._rotation ),
+      _hasChanged( other._hasChanged )
+{
+}
+
+inline
+Transform::~Transform()
+{
+}
+
+// OPERATORS
+inline
+Transform& Transform::operator=( const Transform& other )
+{
+    _matrix = other._matrix;
+    _position = other._position;
+    _rotation = other._rotation;
+    _scale = other._scale;
+    _hasChanged = other._hasChanged;
+
+    return *this;
+}
+
+// ACCESSOR FUNCTIONS
+inline
+const glm::mat4& Transform::matrix() const
+{
+    return _matrix;
+}
+
+inline
+const glm::vec3& Transform::position() const
+{
+    return _position;
+}
+
+inline
+const glm::quat& Transform::rotation() const
+{
+    return _rotation;
+}
+
+inline
+glm::vec3 Transform::eulerRotation() const
+{
+    return glm::eulerAngles( _rotation );
+}
+
+inline
+glm::vec3 Transform::forward() const
+{
+    glm::vec4 dir = _rotation * glm::vec4( 0.0f, 0.0f, 1.0f, 1.0f );
+    return glm::normalize( glm::vec3( dir ) );
+}
+
+inline
+glm::vec3 Transform::up() const
+{
+    glm::vec4 dir = _rotation * glm::vec4( 0.0f, 1.0f, 0.0f, 1.0f );
+    return glm::normalize( glm::vec3( dir ) );
+}
+
+inline
+glm::vec3 Transform::right() const
+{
+    glm::vec4 dir = _rotation * glm::vec4( 1.0f, 0.0f, 0.0f, 1.0f );
+    return glm::normalize( glm::vec3( dir ) );
+}
+
+inline
+float Transform::scale() const
+{
+    return _scale;
+}
+
+inline
+bool Transform::hasChanged() const
+{
+    return _hasChanged;
+}
+
+// MUTATOR FUNCTIONS
+inline
+void Transform::setPosition( const glm::vec3& position )
+{
+    _position = position;
+    recomputeMatrix();
+}
+
+inline
+void Transform::setRotation( const glm::quat& rotation )
+{
+    _rotation = rotation;
+    recomputeMatrix();
+}
+
+inline
+void Transform::setEulerRotation( float x, float y, float z )
+{
+    setEulerRotation( glm::vec3( x, y, z ) );
+}
+
+inline
+void Transform::setScale( float scale )
+{
+    _scale = scale;
+    recomputeMatrix();
+}
+
+// MEMBER FUNCTIONS
+inline
+void Transform::translate( const glm::vec3& translation )
+{
+    _position += translation;
+    recomputeMatrix();
+}
+
+inline
+void Transform::rotate( const glm::quat& rotation )
+{
+    _rotation *= rotation;
+    recomputeMatrix();
+}
+
+inline
+void Transform::rotateEuler( float x, float y, float z )
+{
+    rotateEuler( glm::vec3( x, y, z ) );
+}
+
+inline
+void Transform::scale( float scale )
+{
+    _scale *= scale;
+    recomputeMatrix();
+}
+
+inline
+void Transform::resetChangedFlag()
+{
+    _hasChanged = false;
+}
 
 } // End nspc obj
 
