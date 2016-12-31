2e0077bde13eaf2430de88687bf919241ee23776
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2016-12-31 00:45:09 -0500

    Initialize the graphics api in the demo start up sequence.

--- demo1/src/demo/demo.cpp
+++ demo1/src/demo/demo.cpp
@@ -1,11 +1,15 @@
 // demo.cpp
 #include "demo.h"
 
+#include "demo/render/grapi.h"
+
 namespace demo
 {
 
 void Demo::startup()
 {
+    // initialize the components
+    GrApi::init();
 }
 
 void Demo::run()
