3e5ccec7d4bfe781fccb0b0347d370671ca5c7dd
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2017-01-02 15:56:33 -0500

    Populate camera with methods.

--- demo1/src/demo/object/camera.cpp
+++ demo1/src/demo/object/camera.cpp
@@ -1,2 +1,21 @@
 // camera.cpp
 #include "camera.h"
+
+namespace demo
+{
+
+namespace obj
+{
+
+// OPERATORS
+Camera& Camera::operator=( const Camera& other )
+{
+    _fieldOfView = other._fieldOfView;
+    _nearPlane = other._nearPlane;
+    _farPlane = other._farPlane;
+    return *this;
+}
+
+} // End nspc obj
+
+} // End nspc demo
--- demo1/src/demo/object/camera.h
+++ demo1/src/demo/object/camera.h
@@ -5,16 +5,32 @@
 #ifndef DEMO_CAMERA_H
 #define DEMO_CAMERA_H
 
+#include "demo/object/object.h"
+
 namespace demo
 {
 
 namespace obj
 {
 
-class Camera
+class Camera : public Object
 {
   private:
+    // MEMBERS
+    /**
+     * The field of view.
+     */
+    float _fieldOfView;
 
+    /**
+     * The near plane distance.
+     */
+    float _nearPlane;
+
+    /**
+     * The far plane distance.
+     */
+    float _farPlane;
 
   public:
     // CONSTRUCTORS
@@ -32,7 +48,7 @@ class Camera
     /**
      * Destruct the camera.
      */
-    ~Camera();
+    virtual ~Camera();
 
     // OPERATORS
     /**
@@ -43,30 +59,6 @@ class Camera
     Camera& operator=( const Camera& other );
 
     // ACCESSOR FUNCTIONS
-    /**
-     * Get the position.
-     * @return The position.
-     */
-    void* position() const;
-
-    /**
-     * Get the forward/look direction.
-     * @return The unit directional vector.
-     */
-    void* forward() const;
-
-    /**
-     * Get the viewing up direction.
-     * @return The unit directional vector.
-     */
-    void* up() const;
-
-    /**
-     * Get the viewing right direction.
-     * @return The unit directional vector.
-     */
-    void* right() const;
-
     /**
      * Get the field of view in degrees.
      * @return The field of view.
@@ -87,11 +79,79 @@ class Camera
 
     // MUTATOR FUNCTIONS
     /**
-     * Set the position.
-     * @param position
+     * Set the field of view.
+     * @param fieldOfView The field of view.
      */
-    void setPosition( void* position );
+    void setFieldOfView( float fieldOfView );
+
+    /**
+     * Set the near plane distance.
+     * @param distance The distance.
+     */
+    void setNearPlane( float distance );
+
+    /**
+     * Set the far plane distance.
+     * @param distance The distance.
+     */
+    void setFarPlane( float distance );
 };
+
+// CONSTRUCTORS
+inline
+Camera::Camera() : _fieldOfView(), _nearPlane(), _farPlane()
+{
+}
+
+inline
+Camera::Camera( const Camera& other )
+        : _fieldOfView( other._fieldOfView ), _nearPlane( other._nearPlane ),
+          _farPlane( other._farPlane )
+{
+}
+
+inline
+Camera::~Camera()
+{
+}
+
+// ACCESSOR FUNCTIONS
+inline
+float Camera::fieldOfView() const
+{
+    return _fieldOfView;
+}
+
+inline
+float Camera::nearPlane() const
+{
+    return _nearPlane;
+}
+
+inline
+float Camera::farPlane() const
+{
+    return _farPlane;
+}
+
+// MUTATOR FUNCTIONS
+inline
+void Camera::setFieldOfView( float fieldOfView )
+{
+    _fieldOfView = fieldOfView;
+}
+
+inline
+void Camera::setNearPlane( float distance )
+{
+    _nearPlane = distance;
+}
+
+inline
+void Camera::setFarPlane( float distance )
+{
+    _farPlane = distance;
+}
 
 } // End nspc obj
 
