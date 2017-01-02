dab1e2152406bbfc69b2f5c673446ecdbdcabddc
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2017-01-02 16:43:18 -0500

    Add window management functions.

--- demo1/src/demo/render/window.cpp
+++ demo1/src/demo/render/window.cpp
@@ -13,6 +13,47 @@ void Window::activateTarget()
     glfwMakeContextCurrent( _window );
 }
 
+// MEMBER FUNCTIONS
+void Window::open()
+{
+    if ( isOpen() )
+    {
+        return;
+    }
+
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
+    _window = glfwCreateWindow( 800, 600, _title.c_str(), nullptr, nullptr );
+}
+
+void Window::update()
+{
+    if ( !isOpen() )
+    {
+        return;
+    }
+
+    glfwGetWindowSize( _window, &_width, &_height );
+
+    // if the window should close, close it
+    if ( glfwWindowShouldClose( _window ) )
+    {
+        glfwDestroyWindow( _window );
+        _window = nullptr;
+    }
+}
+
+void Window::close()
+{
+    if ( !isOpen() )
+    {
+        return;
+    }
+
+    glfwDestroyWindow( _window );
+    _window = nullptr;
+}
+
 } // End nspc rndr
 
 } // End nspc demo
--- demo1/src/demo/render/window.h
+++ demo1/src/demo/render/window.h
@@ -7,6 +7,7 @@
 
 #include "demo/render/grapi.h"
 #include "demo/render/rendertarget.h"
+#include "demo/strdef.h"
 
 namespace demo
 {
@@ -22,6 +23,21 @@ class Window : public RenderTarget
      * The window.
      */
     GLFWwindow* _window;
+
+    /**
+     * The window title.
+     */
+    String _title;
+
+    /**
+     * The width.
+     */
+    int32 _width;
+
+    /**
+     * The height.
+     */
+    int32 _height;
 
     // HIDDEN FUNCTIONS
     /**
@@ -52,17 +68,165 @@ class Window : public RenderTarget
      * Destruct the window.
      */
     virtual ~Window();
+
+    // ACCESSOR FUNCTIONS
+    /**
+     * Get the title.
+     * @return The title.
+     */
+    const String& title() const;
+
+    /**
+     * Get the width.
+     * @return The width.
+     */
+    int32 width() const;
+
+    /**
+     * Get the height.
+     * @return The height.
+     */
+    int32 height() const;
+
+    // MUTATOR FUNCTIONS
+    /**
+     * Set the title.
+     * @param title The title.
+     */
+    void setTitle( const String& title );
+
+    /**
+     * Set the width.
+     * @param width The width.
+     */
+    void setWidth( int32 width );
+
+    /**
+     * Set the height.
+     * @param height The height.
+     */
+    void setHeight( int32 height );
+
+    /**
+     * Set the size.
+     * @param width The width.
+     * @param height The height.
+     */
+    void setSize( int32 width, int32 height );
+
+    // MEMBER FUNCTIONS
+    /**
+     * Open the window.
+     * This does nothing if the window is already open.
+     */
+    void open();
+
+    /**
+     * Poll for window events.
+     * This does nothing if the window is not open.
+     */
+    void update();
+
+    /**
+     * Close the window.
+     * This does nothing if the window is not open.
+     * The size will be invalid after the window is closed.
+     */
+    void close();
+
+    /**
+     * Check if the window is visible and open.
+     * @return Is it open?
+     */
+    bool isOpen() const;
 };
 
 // CONSTRUCTORS
 inline
-Window::Window() : _window( nullptr )
+Window::Window() : _window( nullptr ), _width(), _height()
 {
 }
 
 inline
 Window::~Window()
 {
+    if ( _window != nullptr )
+    {
+        glfwDestroyWindow( _window );
+    }
+}
+
+// ACCESSOR FUNCTIONS
+inline
+const String& Window::title() const
+{
+    return _title;
+}
+
+inline
+int32 Window::width() const
+{
+    return _width;
+}
+
+inline
+int32 Window::height() const
+{
+    return _height;
+}
+
+
+// MUTATOR FUNCTIONS
+inline
+void Window::setTitle( const String& title )
+{
+    if ( isOpen() )
+    {
+        glfwSetWindowTitle( _window, title.c_str() );
+    }
+
+    _title = title;
+}
+
+inline
+void Window::setHeight( int32 height )
+{
+    if ( isOpen() )
+    {
+        glfwSetWindowSize( _window, _width, height );
+    }
+
+    _height = height;
+}
+
+inline
+void Window::setWidth( int32 width )
+{
+    if ( isOpen() )
+    {
+        glfwSetWindowSize( _window, width, _height );
+    }
+
+    _width = width;
+}
+
+inline
+void Window::setSize( int32 width, int32 height )
+{
+    if ( isOpen() )
+    {
+        glfwSetWindowSize( _window, width, height );
+    }
+
+    _width = width;
+    _height = height;
+}
+
+// MEMBER FUNCTIONS
+inline
+bool Window::isOpen() const
+{
+    return _window != nullptr;
 }
 
 } // End nspc rndr
