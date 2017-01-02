6ee3af4e0518f2a1d7323d940981d5f6052e7fc0
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2017-01-02 14:49:32 -0500

    Request a specific OpenGL version.

--- demo1/main.cpp
+++ demo1/main.cpp
@@ -3,23 +3,22 @@
 #include "demo/render/grapi.h"
 #include "demo/render/shader.h"
 
-#define GLFW_DLL 
-#include <GLFW/glfw3.h>
-
 int main()
 {
     using namespace demo;
+    rndr::GrApi::init();
 
-    glfwInit();
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
     GLFWwindow* win = glfwCreateWindow( 300, 400, "test", nullptr, nullptr );
     glfwMakeContextCurrent( win );
 
-    printf("OpenGL version is (%s)\n", glGetString(GL_VERSION));
+    printf( "OpenGL version is (%s)\n", glGetString( GL_VERSION ) );
 
     rndr::Shader test( "test1" );
     test.load();
     std::cout << "Hello, World!" << std::endl;
 
-    glfwTerminate();
+    rndr::GrApi::terminate();
     return 0;
 }
