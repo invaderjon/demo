2a6ddf353e4681e8ab940e9833a5627bab43c3b5
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2017-01-12 21:31:13 -0500

    Force only OpenGL 4.1

--- demo1/src/demo/render/window.cpp
+++ demo1/src/demo/render/window.cpp
@@ -24,6 +24,7 @@ void Window::open()
 
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
     glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
     _window = glfwCreateWindow( _width, _height, _title.c_str(), nullptr,
                                 nullptr );
 }
